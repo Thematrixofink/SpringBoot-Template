ddf2aa905d9889cdb7b477b956dc68839fe4db55
Inkslab <12992000+zrinkslab@user.noreply.gitee.com>
2024-09-13 11:17:58 +0800

    init:update readme

---
+++ src/main/java/com/inkslab/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.inkslab.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.inkslab.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
